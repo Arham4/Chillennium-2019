b8d91bb0fd7f0e5b87cf5de2b4e25bfbb0175e85
Arham4 <arhamjsiddiqui@gmail.com>
2019-10-05 18:35:51 -0500

    Refactor project

--- Assets/Scripts/Menu/ZombieDecoration.cs
+++ Assets/Scripts/Menu/ZombieDecoration.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZomMenu : MonoBehaviour
+public class ZombieDecoration : MonoBehaviour
 {
     private int counter = 0;
     private bool up;
