1105590865319282c31db49c34d0a59e59ade738
daps1120 <47128270+daps1120@users.noreply.github.com>
2019-10-05 03:27:37 -0500

    Create Menu Screen

---
+++ Assets/ZomMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZomMenu : MonoBehaviour
+{
+    private int counter = 0;
+    private bool up;
+    private float speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (up)
+        {
+            counter++;
+            transform.Translate(0, counter / speed, 0);
+        }
+        else
+        {
+            counter--;
+            transform.Translate(0, counter / speed, 0);
+        }
+
+        if (counter > 5)
+        {
+            counter = 0;
+            up = false;
+        }
+        if (counter < -5)
+        {
+            counter = 0;
+            up = true;
+        }
+
+    }
+}
