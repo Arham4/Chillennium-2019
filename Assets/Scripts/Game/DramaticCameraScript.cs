9419258c9a30d7300c8eaff8652e90fbdbe93561
unknown <arhamjsiddiqui@gmail.com>
2019-10-05 03:30:34 -0500

    Make passenger translation and game state

---
+++ Assets/Scripts/Game/DramaticCameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Game
+{
+    public class DramaticCameraScript : MonoBehaviour
+    {
+        private Translation _translation;
+
+        private void Start()
+        {
+            var driverTransform = GameObject.Find("Driver").transform;
+            var cameraObject = gameObject;
+            _translation = new Translation(cameraObject,
+                driverTransform.position + new Vector3(0, driverTransform.lossyScale.y * 0.65f, -5), 40);
+        }
+
+        private void Update()
+        {
+            _translation.Execute(then: () =>
+            {
+                GameSingleton.Instance.currentView = View.Driver;
+                Debug.Log("current view is now Driver");
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class StartMenu : MonoBehaviour
+    {
+        public void StartButton()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
