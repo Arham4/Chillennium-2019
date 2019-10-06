873db093f1ec233052e58b6cea048a0ab55bd025
daps1120 <47128270+daps1120@users.noreply.github.com>
2019-10-05 21:51:07 -0500

    Added a timer to day section, and I added a night time scence

---
+++ Assets/Scripts/Game/DriveCompleted.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DriveCompleted : MonoBehaviour
+{
+    //Checks to see when the level is over
+    public float timeLeft = 10;
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("NightTime");
+            Debug.Log("MY MOM");
+        }
+
+    }
+}
