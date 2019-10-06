12cf77f299e9eecebbd911a76a191ee2c676581b
daps1120 <47128270+daps1120@users.noreply.github.com>
2019-10-06 14:57:40 -0500

    Test out stuff

--- Assets/Scripts/Game/Environment/SpawnNature.cs
+++ Assets/Scripts/Game/Environment/SpawnNature.cs
@@ -8,32 +8,35 @@ public class SpawnNature : MonoBehaviour
     public GameObject Trees;
     private bool spawnObject = true;
     public bool mountain = false;
+    public bool sign = false;
+    public float ObstacleSpeed = 2.5f;
+
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 5f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - ObstacleSpeed);
 
-        if(transform.position.z < -60)
-        {
-            if(mountain == false)
-                transform.position = new Vector3(507.2f + Random.Range(-268f, 332f), transform.position.y, 1000f + Random.Range(0, 750f));
-            else
-                transform.position = new Vector3(transform.position.x, transform.position.y, 1000f);
-        }
-
-        var posX = GameObject.Find("Car").transform.position;
-        var gObj = GameObject.Find("Car");
-
-        if (gObj)
-        {
-            posX = gObj.transform.position;
-
-            if (transform.position.x - posX.x <= 15f && transform.position.x - posX.x >= -15f)
+            if (transform.position.z < -60)
             {
-                if (transform.position.z - posX.z <= 3f && transform.position.z - posX.z >= -3f)
+                if (mountain == false)
+                    transform.position = new Vector3(507.2f + Random.Range(-268f, 332f), transform.position.y, 1000f + Random.Range(0, 750f));
+                else
+                    transform.position = new Vector3(transform.position.x, transform.position.y, 1000f);
+            }
+
+            var posX = GameObject.Find("Car").transform.position;
+            var gObj = GameObject.Find("Car");
+
+            if (gObj)
+            {
+                posX = gObj.transform.position;
+
+                if (transform.position.x - posX.x <= 15f && transform.position.x - posX.x >= -15f)
                 {
-                    SceneManager.LoadScene("GameOver");
+                    if (transform.position.z - posX.z <= 3f && transform.position.z - posX.z >= -3f)
+                    {
+                        SceneManager.LoadScene("GameOver");
+                    }
                 }
             }
-        }
     }
 }
---
+++ Assets/warningScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game;
+
+public class warningScript : MonoBehaviour
+{
+    private GameObject warning;
+
+    private void Start()
+    {
+        warning = GameObject.Find("Warning");
+    }
+
+    void Update()
+    {
+        if (GameSingleton.Instance.currentView != View.Backseat)
+        {
+            warning.SetActive(false);
+        }
+        else
+        {
+            //Collision is coming
+            var posX = GameObject.Find("Obstacle").transform.position;
+            var gObj = GameObject.Find("Obstacle");
+
+            if (gObj)
+            {
+                posX = gObj.transform.position;
+                var diff = transform.position.x - posX.x;
+
+                if (transform.position.x - posX.x <= 15f && transform.position.x - posX.x >= -15f)
+                {
+                    transform.position = new Vector3(transform.position.x + diff, transform.position.y, transform.position.z);
+                    warning.SetActive(true);
+                }
+            }
+        }
+    }
+}
